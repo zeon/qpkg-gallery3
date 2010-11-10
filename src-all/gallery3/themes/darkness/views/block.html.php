fba72ab8e3e63ee6988a758d982c2910018733db
zeon <zeonism@gmail.com>
2010-11-10 21:43:56 +0800

    Removed third-party modules and some non-standard themes.

--- src-all/gallery3/themes/darkness/views/block.html.php
+++
@@ -1,8 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
-<a name="<?= $anchor ?>"></a>
-<div id="<?= $css_id ?>" class="gBlock">
-  <h2><?= $title ?></h2>
-  <div class="gBlockContent">
-    <?= $content ?>
-  </div>
-</div>
